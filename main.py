7012b64af46f992e195a2d74c6bb92d052ebb882
sp3p3x <594b4b@gmail.com>
2023-09-01 19:21:11 +0530

    feat: added binwalk

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import os, subprocess
+
+tempPath = "foo.png"
+
 banner = '''
 ███████╗████████╗███████╗ ██████╗  █████╗ ██╗     ██╗      ██████╗ ███████╗██╗████████╗
 ██╔════╝╚══██╔══╝██╔════╝██╔════╝ ██╔══██╗██║     ██║     ██╔═══██╗██╔════╝██║╚══██╔══╝
@@ -9,8 +13,43 @@ banner = '''
 -----------------------Developed & Maintained by @sp3p3x @ask--------------------------
 '''
 
+def binwalk(path):
+    output = os.system("binwalk " + path)
+    print(output)
+
+def pngSteg(path):
+    pass
+
+def jpgSteg(path):
+    pass
+
+def imageSteg(path):
+    binwalk(path)
+
+def audioSteg(path):
+    pass
+
 def main():
-    print(banner)
+    # print(banner)
+
+    while True:
+        # inpFilePath = str(input('Image path: '))
+        inpFilePath = str(tempPath)
+
+        if inpFilePath.endswith('.png'):
+            imageSteg(inpFilePath)
+            pngSteg(inpFilePath)
+        elif inpFilePath.endswith('.jpg') or inpFilePath.endswith('.jpeg'):
+            imageSteg(inpFilePath)
+            jpgSteg(inpFilePath)
+        elif inpFilePath.endswith('.wav') or inpFilePath.endswith('.mp3')  or inpFilePath.endswith('.mp3'):
+            audioSteg(inpFilePath)
+        else:
+            print("Please enter a valid file path!")
+            continue
+
+        break
+
 
 if __name__ == "__main__":
     main()
